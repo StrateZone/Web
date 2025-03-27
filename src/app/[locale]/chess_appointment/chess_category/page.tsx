068344f7e9eee891467c9862977039b36bc27184
focus_On_Huy <pleasestayfocus@gmail.com>
2025-03-27 09:57:37 +0700

    add validate for filter

--- src/app/[locale]/chess_appointment/chess_category/page.tsx
+++ src/app/[locale]/chess_appointment/chess_category/page.tsx
@@ -98,7 +98,7 @@ export default function ChessCategoryPage() {
             indexes: null, // Ngăn Axios thêm [] vào key
             encode: (param) => param, // Tắt auto-encode
           },
-        },
+        }
       );
       console.log("API Response:", response.data);
 
@@ -136,6 +136,10 @@ export default function ChessCategoryPage() {
             "The minimum duration between start and end time is 30 minutes."
           ) {
             message = "Vui lòng chọn thời gian cách nhau 1 giờ!";
+          } else if (message === "Can not select time in the past.") {
+            // message = "Không thể chọn thời gian đã qua trong ngày!";
+            message =
+              "Giờ Bắt Đầu phải sớm hơn giờ kết thúc và Giờ Kết Thúc đã qua trong ngày hôm nay !";
           }
           toast.error(message);
         } else if (errorData?.errors) {
@@ -159,7 +163,7 @@ export default function ChessCategoryPage() {
 
     const now = new Date();
     if (selectedStartTime < now) {
-      toast.error("Giờ bắt đầu phải lớn hơn thời gian hiện tại!");
+      toast.error("Giờ bắt đầu đã qua, vui lòng chọn thời gian hợp lệ!");
       return;
     }
 
@@ -401,7 +405,7 @@ export default function ChessCategoryPage() {
                         <span className="font-medium text-black text-sm ml-1">
                           (
                           {Number(chessBooking.gameTypePrice).toLocaleString(
-                            "vi-VN",
+                            "vi-VN"
                           )}{" "}
                           ₫/giờ)
                         </span>
@@ -421,7 +425,7 @@ export default function ChessCategoryPage() {
                         <span className="font-medium text-black text-sm ml-1">
                           (
                           {Number(chessBooking.roomTypePrice).toLocaleString(
-                            "vi-VN",
+                            "vi-VN"
                           )}{" "}
                           ₫/giờ)
                         </span>
@@ -434,7 +438,7 @@ export default function ChessCategoryPage() {
                       <p className="text-gray-600 text-sm mt-2">
                         <span className="font-medium text-black">Ngày: </span>{" "}
                         {new Date(chessBooking.startDate).toLocaleDateString(
-                          "vi-VN",
+                          "vi-VN"
                         )}
                       </p>
                       <p className="text-gray-600 text-sm mt-2">
@@ -446,7 +450,7 @@ export default function ChessCategoryPage() {
                             minute: "2-digit",
                             hour12: false,
                             timeZone: "Asia/Ho_Chi_Minh",
-                          },
+                          }
                         )}
                         {" giờ"}
                         <span className="font-medium text-black">
@@ -460,7 +464,7 @@ export default function ChessCategoryPage() {
                             minute: "2-digit",
                             hour12: false,
                             timeZone: "Asia/Ho_Chi_Minh",
-                          },
+                          }
                         )}{" "}
                         {" giờ"}
                       </p>
@@ -495,8 +499,8 @@ export default function ChessCategoryPage() {
                             } else {
                               router.push(
                                 `/${locale}/chess_appointment/${chessBooking.tableId}?startTime=${encodeURIComponent(
-                                  chessBooking.startDate,
-                                )}&endTime=${encodeURIComponent(chessBooking.endDate)}`,
+                                  chessBooking.startDate
+                                )}&endTime=${encodeURIComponent(chessBooking.endDate)}`
                               );
                             }
                           }}
