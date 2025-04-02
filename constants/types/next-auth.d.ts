d6ec6de13599d9dcf616bb50bc9173023e8e91c3
focus_On_Huy <pleasestayfocus@gmail.com>
2025-04-02 22:51:46 +0700

    fix eslint 3

--- constants/types/next-auth.d.ts
+++ constants/types/next-auth.d.ts
@@ -1,23 +1,23 @@
-// import NextAuth from "next-auth";
+import NextAuth from "next-auth";
 
-// export type User = {
-//   id: string;
-//   email: string;
-//   role: string;
-//   fullName: string;
-//   accessToken: string;
-// };
+export type User = {
+  id: string;
+  email: string;
+  role: string;
+  fullName: string;
+  accessToken: string;
+};
 
-// declare module "next-auth" {
-//   interface Session {
-//     user: User;
-//   }
+declare module "next-auth" {
+  interface Session {
+    user: User;
+  }
 
-//   interface User {
-//     id: string;
-//     email: string;
-//     role: string;
-//     fullName: string;
-//     accessToken: string;
-//   }
-// }
+  interface User {
+    id: string;
+    email: string;
+    role: string;
+    fullName: string;
+    accessToken: string;
+  }
+}
